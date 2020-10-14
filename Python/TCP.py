f75cec8fc7fdb36d5eff3110c972bd702a7f7f8d
subbhashit mukherjee <subbhashitmukherjee@gmail.com>
2020-10-14 21:19:21 +0530

    Sniff update

--- Python/TCP.py
+++ Python/TCP.py
@@ -1,6 +1,13 @@
 import sys
-
+import socket
+# import dpkt, pcap
 def TCP():
     print(sys.argv[1])
 print("Output from Python")
 TCP()
+
+
+pc = pcap.pcap()     # construct pcap object
+pc.setfilter('icmp') # filter out unwanted packets
+for timestamp, packet in pc:
+    print(dpkt.ethernet.Ethernet(packet))
