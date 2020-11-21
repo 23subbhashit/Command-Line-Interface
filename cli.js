af10b4f7388964e3c0b8336e1c104a94f694ff86
Subbhashit <subbhashitmukherjee@gmail.com>
2020-11-21 10:51:58 +0530

    Added Support for final

--- cli.js
+++ cli.js
@@ -3,6 +3,7 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
+const chaklani = require('chalk-animation');
 
 // import libs files here
 
@@ -24,25 +25,35 @@ const run = async () => {
     console.log(credentials);
     if(credentials['username']=='TCP'){
     var spawn = require('child_process').spawn;
-    const py = spawn('python', ['./Python/TCP.py',credentials['data']]);
+    const py = spawn('python3', ['./Python/TCP.py',credentials['data']]);
     py.stdout.on('data' , data =>{
       console.log(data.toString());
     });
   }
   else if(credentials['username']=='UDP'){
     var spawn = require('child_process').spawn;
-    const py = spawn('python', ['./Python/UDP.py',credentials['data']]);
+    const py = spawn('python3', ['./Python/UDP.py',credentials['data']]);
     py.stdout.on('data' , data =>{
       console.log(data.toString());
     });
   }
   else if(credentials['username']=='ICMP'){
     var spawn = require('child_process').spawn;
-    const py = spawn('python', ['./Python/ICMP.py',credentials['data']]);
+    const py = spawn('python3', ['./Python/ICMP.py',credentials['data']]);
+    py.stdout.on('data' , data =>{
+      console.log(data.toString());
+
+    });
+  }
+  else if(credentials['username']=='Final'){
+    var spawn = require('child_process').spawn;
+    const py = spawn('python3', ['./Python/Final.py',credentials['data']]);
     py.stdout.on('data' , data =>{
       console.log(data.toString());
     });
   }
+  
+    
   };
 
   
