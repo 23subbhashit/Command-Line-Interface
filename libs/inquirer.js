06f08b398681677d398f05723170521858d840c1
23subbhashit <subbhashitmukherjee@gmail.com>
2020-11-24 20:02:31 +0530

    Test 1 using mocha chai

--- libs/inquirer.js
+++ libs/inquirer.js
@@ -16,19 +16,8 @@ module.exports = {
           }
         }
       },
-      // {
-      //   name: 'data',
-      //   type: 'password',
-      //   message: 'Enter your data to be sniffed:',
-      //   validate: function(value) {
-      //     if (value.length) {
-      //       return true;
-      //     } else {
-      //       return 'Please reenter your data.';
-      //     }
-      //   }
-      // }
     ];
+    
     return inquirer.prompt(questions);
   },
 };
--- test/appTest.js
+++ test/appTest.js
@@ -1,15 +1,37 @@
 const expect = require('chai').expect; 
+const inquirer  = require('../libs/inquirer');
 
-describe("Testing with chai", () => { 
-	it("Is returning 4 when adding 2 + 2", () => { 
-	expect(2 + 2).to.equal(4); 
+describe("Testing Command Line Interface using chai", () => { 
+	async function wait(ms) {
+		return new Promise(resolve => {
+		  setTimeout(resolve, ms);
+		});
+	}
+	it("Testing prompt", async() => {	
+		
+	// await wait(5000);
+	const credentials = await inquirer.askSniffCredentials();
+	
+	console.log(credentials);
+	if(credentials['method']=='TCP'){
+		expect(credentials).to.eql({ method: 'TCP' });
+	}
+	if(credentials['method']=='UDP'){
+		expect(credentials).to.eql({ method: 'UDP' });
+	}
+	if(credentials['method']=='ICMP'){
+		expect(credentials).to.eql({ method: 'ICMP' });
+	}
+	if(credentials['method']=='Final'){
+		expect(credentials).to.eql({ method: 'Final' });
+	}
 	}); 
 
-	it("Is returning boolean value as true", () => { 
+	it("Is returning boolean value as true", async() => { 
 	expect(5 == 5).to.be.true; 
 	}); 
 	
-	it("Are both the sentences matching", () => { 
+	it("Are both the sentences matching", async() => { 
 	expect("This is working").to.equal('This is working'); 
 	}); 
 }); 
